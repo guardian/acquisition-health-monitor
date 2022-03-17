fc8117660630c738aeef524bbf15e33baabd01d4
John Duffell <john.duffell@guardian.co.uk>
2022-03-17 18:26:22 +0000

    split aws cloudwatch response into separate values

--- app/com/gu/acquisition_health_monitor/aws/AwsCloudWatchMetricGet.scala
+++ app/com/gu/acquisition_health_monitor/aws/AwsCloudWatchMetricGet.scala
@@ -1,15 +1,14 @@
 package com.gu.acquisition_health_monitor.aws
 
-import com.gu.acquisition_health_monitor.aws.AwsCloudWatch.{MetricDimensionName, MetricDimensionValue, MetricName, MetricNamespace, MetricPeriod, MetricRequest, MetricStats, buildMetricRequest}
-import software.amazon.awssdk.auth.credentials.{AwsCredentialsProviderChain, EnvironmentVariableCredentialsProvider, ProfileCredentialsProvider}
+import com.gu.acquisition_health_monitor.aws.AwsCloudWatch.{MetricRequest, MetricRequestBuilder}
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain
 import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient
-import software.amazon.awssdk.services.cloudwatch.model.{Dimension, DimensionFilter, GetMetricDataRequest, GetMetricDataResponse, GetMetricStatisticsRequest, ListMetricsRequest, ListMetricsResponse, Metric, MetricDataQuery, MetricDataResult, MetricDatum, MetricStat, PutMetricDataRequest, StandardUnit}
+import software.amazon.awssdk.services.cloudwatch.model._
 
-import scala.jdk.CollectionConverters._
 import java.time.Instant
-import scala.collection.View.Empty
-import scala.util.{Failure, Success, Try}
+import scala.jdk.CollectionConverters._
+import scala.util.Try
 
 
 
@@ -22,34 +21,31 @@ class AwsCloudWatch(credential:  AwsCredentialsProviderChain) {
 
   println(s"client: ${client}")
 
-  private def listMetrics(): Either[String, ListMetricsResponse] = {
+  private def listPaymentSuccessMetrics(): Either[String, ListMetricsResponse] = {
     val listMetricsRequest: ListMetricsRequest = ListMetricsRequest.builder()
       .namespace("support-frontend")
       .metricName("PaymentSuccess")
       .dimensions(DimensionFilter.builder.name("Stage").value("PROD").build())
-      .build();
-    val failableResult = Try {
+      .build()
+    Try {
       client.listMetrics(listMetricsRequest)
     }.toEither.left.map(x => x.toString)
-
-    failableResult
   }
 
-  def getAllMetrics(request: MetricRequest): Either[String, List[Map[Instant, Double]]]= {
+  def getAllPaymentSuccessMetrics(request: MetricRequest): Either[String, Map[String, Map[Instant, Double]]]= {
     for {
-      listOfAllMetrics <- listMetrics()
-      allMetrics <- metricGet(request, None, listOfAllMetrics, Nil)
-    } yield AwsToScala(allMetrics)
+      listOfAllMetrics <- listPaymentSuccessMetrics()
+      allMetrics <- getMetricDataFromMetrics(new MetricRequestBuilder(request, listOfAllMetrics), None, Nil)
+    } yield groupMetricDataByProduct(allMetrics)
   }
 
-  def metricGet(
-    request: MetricRequest,
+  def getMetricDataFromMetrics(
+    metricRequestBuilder: MetricRequestBuilder,
     nextToken: Option[String],
-    listMetricsResponse: ListMetricsResponse,
     soFar: List[GetMetricDataResponse]
   ): Either[String, List[GetMetricDataResponse]] = {
 
-    val metricDataRequest: GetMetricDataRequest = buildMetricRequest(request, listMetricsResponse, nextToken)
+    val metricDataRequest: GetMetricDataRequest = metricRequestBuilder.build(nextToken)
 
     val failableResult = Try {
       client.getMetricData(metricDataRequest)
@@ -59,25 +55,32 @@ class AwsCloudWatch(credential:  AwsCredentialsProviderChain) {
       value <- failableResult
       metricResults <- Option(value.nextToken) match {
         case Some(next) =>
-          metricGet(request, Some(next), listMetricsResponse, value :: soFar)
+          getMetricDataFromMetrics(metricRequestBuilder, Some(next), value :: soFar)
         case None =>
          Right((value :: soFar).reverse)
       }
     } yield metricResults
   }
 
-  private def AwsToScala(responses: List[GetMetricDataResponse]) =
-    for {
-      response <- responses
-      metricResult <- response.metricDataResults().asScala.toList
+  private def groupMetricDataByProduct(responsePages: List[GetMetricDataResponse]) = {
+    val pagesData: List[(String, Map[Instant, Double])] = for {
+      page <- responsePages
+      singleMetricDataForPage <- page.metricDataResults().asScala.toList
     } yield {
-      println("value size: " + metricResult.values.size) // FIXME use the logger properly so it goes into the right place
-      println("The label is " + metricResult.label())
-      println("The status code is " + metricResult.statusCode().toString())
-      val timestamps = metricResult.timestamps().asScala.toList
-      val values = metricResult.values().asScala.toList.map(x => x.toDouble)
-      timestamps.zip(values).toMap
+      println("value size: " + singleMetricDataForPage.values.size) // FIXME use the logger properly so it goes into the right place
+      println("The label is " + singleMetricDataForPage.label())
+      println("The status code is " + singleMetricDataForPage.statusCode().toString())
+      val timestamps = singleMetricDataForPage.timestamps().asScala.toList
+      val values = singleMetricDataForPage.values().asScala.toList.map(x => x.toDouble)
+      val valuesForPage = timestamps.zip(values).toMap
+      val labelForMetric = singleMetricDataForPage.label()
+      (labelForMetric, valuesForPage)
     }
+    // at this point, we have one record for each page and each metric.  So for 2 pages and 4 metrics we would have
+    // 8 records.
+    // next we need to merge together the data with the same label
+    pagesData.groupMapReduce(_._1)(_._2)(_ concat _) // TODO might we get duplicate "Instant"s? check AWS docs
+  }
 
 }
 
@@ -95,7 +98,12 @@ object AwsCloudWatch {
 
   case class MetricPeriod(value: Int) extends AnyVal
 
-  case class MetricRequest(period: MetricPeriod, stat: MetricStats)
+  case class MetricRequest(
+    period: MetricPeriod,
+    stat: MetricStats,
+    start: Instant,
+    endDate: Instant,
+  )
 
   private def getLabelFromDimension(id: Int, dimensions: List[Dimension]) = {
 
@@ -105,35 +113,33 @@ object AwsCloudWatch {
     label1 + "-" + label2
   }
 
-  private[aws] def buildMetricRequest(request: MetricRequest, listMetricResponse: ListMetricsResponse, nextToken: Option[String]): GetMetricDataRequest = {
+  private class MetricRequestBuilder(request: MetricRequest, listMetricResponse: ListMetricsResponse) {
 
-    //println("Now: " + Instant.now())
-    val start = Instant.parse("2022-03-03T10:00:00Z")
-    val endDate = Instant.parse("2022-03-03T16:00:00Z")
+    def build(nextToken: Option[String]): GetMetricDataRequest = {
 
+      val queries = listMetricResponse.metrics().asScala.zipWithIndex.map {
+        case (metric, index) => {
+          val metricStat = MetricStat.builder
+            .stat(request.stat.value)
+            .period(request.period.value)
+            .metric(metric)
+            .build()
 
-    val queries = listMetricResponse.metrics().asScala.zipWithIndex.map {
-      case (metric, index) => {
-        val metricStat = MetricStat.builder
-          .stat(request.stat.value)
-          .period(request.period.value)
-          .metric(metric)
-          .build()
-
-        MetricDataQuery.builder
-          .metricStat(metricStat)
-          .id("id" + index)
-          .label(getLabelFromDimension(index, metric.dimensions().asScala.toList))
-          .returnData(true).build()
+          MetricDataQuery.builder
+            .metricStat(metricStat)
+            .id("id" + index)
+            .label(getLabelFromDimension(index, metric.dimensions().asScala.toList))
+            .returnData(true).build()
+        }
       }
-    }
 
-    GetMetricDataRequest.builder
-      //.maxDatapoints(200)
-      .startTime(start)
-      .endTime(endDate)
-      .metricDataQueries(queries.asJava)
-      .nextToken(nextToken.orNull)
-      .build()
+      GetMetricDataRequest.builder
+        //.maxDatapoints(200)
+        .startTime(request.start)
+        .endTime(request.endDate)
+        .metricDataQueries(queries.asJava)
+        .nextToken(nextToken.orNull)
+        .build()
+    }
   }
 }
