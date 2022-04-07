3680339da0c65b0e07dd832f257812591d38501d
John Duffell <john.duffell@guardian.co.uk>
2022-04-07 17:38:04 +0100

    try out exploratory use of unfold instead of recursion

--- app/com/gu/acquisition_health_monitor/aws/AwsCloudWatchMetricGet.scala
+++ app/com/gu/acquisition_health_monitor/aws/AwsCloudWatchMetricGet.scala
@@ -1,6 +1,6 @@
 package com.gu.acquisition_health_monitor.aws
 
-import com.gu.acquisition_health_monitor.aws.AwsCloudWatch.{MetricRequest, MetricRequestBuilder}
+import com.gu.acquisition_health_monitor.aws.AwsCloudWatch.{Continue, FetchFirstPage, MaybeContinue, MetricRequest, MetricRequestBuilder, PageToFetch, FetchSpecificPage, Terminate}
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain
 import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient
@@ -35,31 +35,65 @@ class AwsCloudWatch(credential:  AwsCredentialsProviderChain) {
   def getAllPaymentSuccessMetrics(request: MetricRequest): Either[String, Map[String, Map[Instant, Double]]]= {
     for {
       listOfAllMetrics <- listPaymentSuccessMetrics()
-      allMetrics <- getMetricDataFromMetrics(new MetricRequestBuilder(request, listOfAllMetrics), None, Nil)
+      allMetrics <- getAllFailablePages(getCloudwatchPage(new MetricRequestBuilder(request, listOfAllMetrics)))
     } yield groupMetricDataByProduct(allMetrics)
   }
 
-  def getMetricDataFromMetrics(
-    metricRequestBuilder: MetricRequestBuilder,
-    nextToken: Option[String],
-    soFar: List[GetMetricDataResponse]
-  ): Either[String, List[GetMetricDataResponse]] = {
+  def getCloudwatchPage(metricRequestBuilder: MetricRequestBuilder)(token: PageToFetch[String]): Either[String, (GetMetricDataResponse, MaybeContinue[String])] =
+  {
+    val metricDataRequest: GetMetricDataRequest = metricRequestBuilder.build(token)
 
-    val metricDataRequest: GetMetricDataRequest = metricRequestBuilder.build(nextToken)
-
-    val failableResult = Try {
+    val failableResult: Either[String, (GetMetricDataResponse, MaybeContinue[String])] = Try {
       client.getMetricData(metricDataRequest)
-    }.toEither.left.map(x => x.toString)
-
-    for {
-      value <- failableResult
-      metricResults <- Option(value.nextToken) match {
-        case Some(next) =>
-          getMetricDataFromMetrics(metricRequestBuilder, Some(next), value :: soFar)
-        case None =>
-         Right((value :: soFar).reverse)
+    }.toEither.left.map(x => x.toString).map(value => {
+      val maybeContinue = Option(value.nextToken) match {
+        case Some(value) => Continue(value)
+        case None => Terminate
       }
-    } yield metricResults
+      (value, maybeContinue)
+    })
+
+    failableResult
+  }
+
+  def getAllFailablePages[RESULT, SEED](
+    block: PageToFetch[SEED] => Either[String, (RESULT, MaybeContinue[SEED])]
+  ): Either[String, List[RESULT]] = {
+
+    val initialSeed: MaybeContinue[PageToFetch[SEED]] = Continue(FetchFirstPage)
+
+    def unfoldSeedOneLevel(action: MaybeContinue[PageToFetch[SEED]]): MaybeContinue[(Either[String, RESULT], MaybeContinue[PageToFetch[SEED]])] =
+      action match {
+        case Terminate => Terminate
+        case Continue(seed) =>
+          val (either, next) = pushEitherToPair(block(seed))
+          val actionOnNextIteration = next match {
+            case Terminate => Terminate
+            case Continue(seed) => Continue(FetchSpecificPage(seed))
+          }
+          Continue((either, actionOnNextIteration))
+      }
+
+    val pages: List[Either[String, RESULT]] =
+      List.unfold(initialSeed)(unfoldSeedOneLevel(_).toOption)
+
+    errorOrElseSuccesses(pages)
+
+  }
+
+  def pushEitherToPair[A, B, C](either: Either[A, (B, MaybeContinue[C])]): (Either[A, B], MaybeContinue[C]) =
+    either match {
+      case Left(error) => // if it failed then return the error and stop
+        (Left(error), Terminate)
+      case Right((value, maybeToken)) =>
+        (Right(value), maybeToken)
+    }
+
+  def errorOrElseSuccesses[A, B](listEither: List[Either[A, B]]): Either[A, List[B]] = {
+    listEither.partitionMap(identity) match {
+      case (error :: _, _) => Left(error)
+      case (Nil, successes) => Right(successes)
+    }
   }
 
   private def groupMetricDataByProduct(responsePages: List[GetMetricDataResponse]) = {
@@ -85,6 +119,14 @@ class AwsCloudWatch(credential:  AwsCredentialsProviderChain) {
 }
 
 object AwsCloudWatch {
+
+  sealed abstract class MaybeContinue[+SEED](val toOption: Option[SEED])
+  case object Terminate extends MaybeContinue[Nothing](None)
+  case class Continue[SEED](seed: SEED) extends MaybeContinue[SEED](Some(seed))
+
+  sealed trait PageToFetch[+TOKEN]
+  case object FetchFirstPage extends PageToFetch[Nothing]
+  case class FetchSpecificPage[TOKEN](token: TOKEN) extends PageToFetch[TOKEN]
 
   case class MetricNamespace(value: String) extends AnyVal
 
@@ -115,7 +157,7 @@ object AwsCloudWatch {
 
   private class MetricRequestBuilder(request: MetricRequest, listMetricResponse: ListMetricsResponse) {
 
-    def build(nextToken: Option[String]): GetMetricDataRequest = {
+    def build(nextToken: PageToFetch[String]): GetMetricDataRequest = {
 
       val queries = listMetricResponse.metrics().asScala.zipWithIndex.map {
         case (metric, index) => {
